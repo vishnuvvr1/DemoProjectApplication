69f41b97b03573fcd482402140debb29eb3bc047
vishnuvvr1 <vishnuvvr8919@gmail.com>
2024-02-07 12:47:59 +0530

    java Spring boot project

--- src/main/java/com/java/vishnu/DemoProject/repository/UserDao.java
+++ src/main/java/com/java/vishnu/DemoProject/repository/UserDao.java
@@ -3,7 +3,20 @@ package com.java.vishnu.DemoProject.repository;
 import com.java.vishnu.DemoProject.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface UserDao extends JpaRepository<User, Long> {
 
     User findByFirstName(String name);
+    List<User> findAllByFirstName(String name);
+
+    List<User> findAllByLastName(String lastName);
+
+    List<User> findAllByEmail(String email);
+
+   List<User> findAllByAge(Integer age);
+
+    User findByDob(Date dob);
 }
+
--- src/main/java/com/java/vishnu/DemoProject/repository/user/UserRepositoryImpl.java
+++ src/main/java/com/java/vishnu/DemoProject/repository/user/UserRepositoryImpl.java
@@ -6,6 +6,8 @@ import com.java.vishnu.DemoProject.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -30,7 +32,30 @@ public class UserRepositoryImpl implements UserRepository {
     }
 
     @Override
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers(Long id, String firstName, String lastName, String email, Integer age, Date dob) {
+        List<User> users = new ArrayList<>();
+        if (id != null) {
+            users.add(userDao.findById(id).get());
+
+            return users;
+        }
+        if (firstName != null) {
+            return userDao.findAllByFirstName(firstName);
+        }
+        if (lastName != null) {
+            return userDao.findAllByLastName(lastName);
+        }
+        if (email != null) {
+            return userDao.findAllByEmail(email);
+
+        }
+        if (age != null) {
+            return userDao.findAllByAge(age);
+        }
+        if (dob != null) {
+            users.add(userDao.findByDob(dob));
+            return users;
+        }
         return userDao.findAll();
     }
 
